43baa7697224b16be030c292357ac055bb3ec08e
lucaspera09 <lucaspera.2001@gmail.com>
2023-09-05 11:54:36 -0300

    final
    entrega final

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 document.getElementById('registroForm').addEventListener('submit', function(event) {
-    event.preventDefault(); //previene el comportamiento predeterminado del formulario de enviar una solicitud HTTP y recargar la página 
-  
-    const formData = {
-      nombre: document.getElementById('nombre').value,
-      apellido: document.getElementById('apellido').value,
-      fechaNacimiento: document.getElementById('fechaNacimiento').value,
-    };
-  
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    };
-  
-    fetch(url, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log('Respuesta del servidor:', data);
-        
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
-  
+  event.preventDefault(); //previene el comportamiento predeterminado del formulario de enviar una solicitud HTTP y recargar la página 
+
+  const formData = {
+    nombre: document.getElementById('nombre').value,
+    apellido: document.getElementById('apellido').value,
+    fechaNacimiento: document.getElementById('fechaNacimiento').value,
+  };
+
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  };
+
+  fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Respuesta del servidor:', data);
+      
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
